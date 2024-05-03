49657d20329ecab57f0e7a7e6f0f86ed980450b9
Justin Shim <justinjaeshim@gmail.com>
2024-05-02 21:46:52 -0700

    Initialize shadcn components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-
+import { Button } from "$lib/components/ui/button";
 </script>
 
 <svelte:head>
@@ -8,8 +8,9 @@
 </svelte:head>
 
 <section>
-	<div class="flex justify-center items-center h-100dvh">
+	<div class="flex flex-col justify-center items-center h-100dvh">
 		<h1>Hi!</h1>
+		<Button class="mt-8" variant="default">This is a button</Button>
 	</div>
 </section>
 
