fd1173b196582ed2cc315c747a95976c5478e378
Justin Shim <justinjaeshim@gmail.com>
2024-05-09 16:30:48 -0700

    Add landing page changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,9 @@ import { Button } from "$lib/components/ui/button";
 
 <section>
 	<div class="flex flex-col justify-center items-center h-100dvh">
-		<h1>Hi!</h1>
+		<h1 class="text-6xl font-bold max-w-[500px] text-center">This is your SaaS Landing Page</h1>
+		<p class="mt-4">Get your application off the ground and running quickly</p>
+
 		<Button class="mt-8" variant="default">This is a button</Button>
 	</div>
 </section>
